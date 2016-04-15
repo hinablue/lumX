aae14fd59f6f4a2845590f44035dbc69efb75e20
Hina Chen <hinablue@gmail.com>
2016-04-15 23:03:07 +0800

    update dist

--- dist/lumx.js
+++ dist/lumx.js
@@ -616,7 +616,7 @@
 
             var previousDay = angular.copy(lxDatePicker.ngModelMoment).date(0);
             var firstDayOfMonth = angular.copy(lxDatePicker.ngModelMoment).date(1);
-            var lastDayOfMonth = firstDayOfMonth.endOf('month');
+            var lastDayOfMonth = firstDayOfMonth.clone().endOf('month');
             var maxDays = lastDayOfMonth.date();
 
             lxDatePicker.emptyFirstDays = [];
@@ -740,6 +740,7 @@
         }
     }
 })();
+
 (function()
 {
     'use strict';
